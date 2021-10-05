17d080bcef221b7db838c3e7c16d87766f410af7
chengweihua <15133266546@sohu.com>
2021-10-06 00:00:40 +0800

    chore:source-map

--- lesson/src/index.js
+++ lesson/src/index.js
@@ -1,16 +1,1 @@
-// ES Moudule模块引入方式
-// import Header from './header.js'
-// import Content from './content.js'
-import wyz from './wyz.jpg'
-import './index.css'
-
-var img = new Image()
-img.src = wyz
-img.classList.add('wyz')
-
-document.getElementById('root');
-root.append(img)
-
-//构造函数
-// new Header();
-// new Content();
+consssssole.log('hello world')
--- lesson/webpack.config.js
+++ lesson/webpack.config.js
@@ -5,11 +5,16 @@ var path = require('path');
 
 // CommonJS语法
 module.exports = {
-  mode: 'production',
+  mode: 'development',
+  devtool: 'source-map',
+  // inline: map文件直接打包到main.js文件中
+  // cheap: 只告知第几行出错, 不告知第几列
+  // module: 业务代码 + 第三方模块报错
+  // eval:通过evel的执行形式,生成对应关系
+
   // entry: "./src/index.js", //开始打包的文件
   entry: {
     main: './src/index.js',
-    sub: './src/index.js'
   },
   module: { //打包模块
     rules: [ //规则,数组类型
@@ -33,7 +38,6 @@ module.exports = {
   output: {
     filename: '[name].js', //打包后的文件名字
     path: path.resolve(__dirname, 'dist'),  //打包后文件位置(绝对路径,注意需要引入webpack核心模块path,通过path.resolve(__dirname, '打包文件夹名称'),__dirname:webpack.config.js所在当前目录路径)
-    publicPath: 'http://cdn.com'
   },
   plugins: [
     new HtmlWebpackPlugin({
