d1fd9b1fb342abb691a56c6a79e59275521d583f
chengweihua <15133266546@sohu.com>
2021-10-05 18:32:02 +0800

    chore :loader使用

--- lesson/src/index.js
+++ lesson/src/index.js
@@ -1,7 +1,14 @@
 // ES Moudule模块引入方式
-import Header from './header.js'
-import Content from './content.js'
+// import Header from './header.js'
+// import Content from './content.js'
+import wyz from './wyz.jpg'
+
+var img = new Image()
+img.src = wyz
+
+document.getElementById('root');
+root.append(img)
 
 //构造函数
-new Header();
-new Content();
+// new Header();
+// new Content();
--- lesson/webpack.config.js
+++ lesson/webpack.config.js
@@ -2,7 +2,23 @@ const path = require('path');
 
 // CommonJS语法
 module.exports = {
+  mode: 'production',
   entry: "./src/index.js", //开始打包的文件
+  module: { //打包模块
+    rules: [ //规则,数组类型
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: {
+          loader: 'url-loader',
+          options: {  //额外参数
+            name: '[name]_[hash].[ext]',  //placeholder 占位符
+            outputPath: 'images',  //打包后文件存放的位置
+            limit: 2048   //图片大小,当图片小于2kb时打包到bundle.js中,否则新建image文件夹存放
+          }
+        }
+      }
+    ]
+  },
   output: {
     filename: 'bundle.js', //打包后的文件名字
     path: path.resolve(__dirname, 'dist'),  //打包后文件位置(绝对路径,注意需要引入webpack核心模块path,通过path.resolve(__dirname, '打包文件夹名称'),__dirname:webpack.config.js所在当前目录路径)
